f3274f7c33dfda4cad2553cf8f5f66fef4f6ca8c
DOIT <109971423+DOIT008@users.noreply.github.com>
2025-11-20 19:02:05 +0800

    refactor(01.dart): 重构代码并移除未使用的类和函数
    移除未使用的Person类、loopList和loopMap函数
    修改sayHello函数以接受参数并演示变量修改
    添加常量PI和weekDays，并更新main函数逻辑

--- 01.dart
+++ 01.dart
@@ -1,43 +1,30 @@
+
 String name = '张三';
 // 定义一个函数
-void sayHello() {
-  print('hello $name');
-}
-// 定义一个list
-List<String> names = ['张三', '李四', '王五'];
-// 定义一个map
-Map<String, int> scores = {'张三': 90, '李四': 80, '王五': 70};
-// 定义一个Object类
-class Person {
-  String name;
-  int age;
-  Person(this.name, this.age);
-  // 定义一个方法
-  void sayName() {
-    print('my name is $name');
-  }
+void sayHello(String localName) {
+  print('修改前 $localName');
+  localName = '李四';
+  print('修改后 $localName');
 }
 
-var person1 = Person('李四', 18);
+const PI = 3.1415926;
+// PI = 123456; // 错误写法
 
-void loopList() {
-  for (var item in names) {
-    print(item);
-  }
-}
-// 定义一个函数，遍历map
-void loopMap() {
-  for (var key in scores.keys) {
-    print('$key: ${scores[key]}');
-  }
-}
+// void 标识main函数没有返回值
+
+final now = DateTime.now().weekday;
+const weekDays = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
+
+// const now1 = DateTime.now();
 
 
-void main() {
-  sayHello();
-  loopList();
-  loopMap();
-  print(person1);
-  print(person1.age);
-  person1.sayName();
+void main() { 
+  // sayHello(name);
+  // print(name);
+  print(weekDays[now - 1]);
 }
+
+/* 注释 */
+
+
+
---
+++ 02.数据类型.dart
@@ -0,0 +1,69 @@
+// var str = 'hello world';
+// String str1 = 'hello world';
+// // 多行字符串
+// String str2 = '''
+// hello world
+// hello world
+// hello world
+// ''';
+// String str3 = """
+// hello world
+// hello world
+// hello world
+// """;
+// String str5 = '你好 Dart';
+// String str6 = '你好 Flutter';
+// String str7 = '$str5$str6';
+// // 等同于
+// String str8 = '${str5}${str6}';
+// 
+// 
+// 
+// // 
+// int a = 10;
+// 
+// double b = 11.1;
+// double c = 12;
+// // 运算符
+// int d = a + 10;
+// double e = b + c;
+// 
+// // 关系运算符
+// bool f = a > b;
+// bool g = c >= d;
+// 
+// // 乘法运算符
+// int h = a * 2;
+// double i = b * 3;
+// 
+// // 除法运算符
+// double j = c / 2;
+// double k = b / a;
+// 
+// // 布尔值
+// bool l = a == 10;
+// bool m = b != c;
+// 
+// 
+// 
+// void main() {
+//   print(a);
+//   print(b);
+//   print(c);
+// }
+
+// -----------------------------------数字类型--------------------------
+
+// var a = 123;
+// var b = '123';
+// 
+// void main(){
+//   if(a==b){
+//     print('a==b');
+//   }else{
+//     print('a!=b'); // 走这里
+//   }
+// }
+
+// -----------------------------------List类型--------------------------
+
